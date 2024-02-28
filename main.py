bff69664016133690b4e313648457bc874537fe4
lorenzoMarquesini <lorenzomarquesini@gmail.com>
2024-02-27 23:11:35 -0300

    Set up

---
+++ main.py
@@ -0,0 +1,29 @@
+# -- Titulo x 2 Segundos --
+
+# -- Mostrar hook antes de transición --
+# Hasta ahora.. Denada... Presta atencion... Te arrepentiras si no lo guardas..
+
+# -- Content items -- 
+# Tip / Info
+# Description
+
+# -- Outro & CTA --
+# Save the video / Comment "Word"
+
+##---------------------------------------------------------------------------
+import random
+
+data = {
+  "tips": [
+    "tip 1",
+    "tip 2",
+    "tip 3",
+    "tip 4",
+    "tip 5",
+    "tip 6",
+    "tip 7",
+    "tip 8",
+    "tip 9",
+    "tip 10",
+  ],
+}
